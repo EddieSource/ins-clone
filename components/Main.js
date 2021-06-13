269af0be49735cd464ddd668d10f7ef66f830a6d
Xianhui Zhu <xz2715@nyu.edu>
2021-06-12 21:40:00 -0400

    initialize redux

---
+++ components/Main.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+
+const Main = () => {
+    useEffect(() => {
+        return () => {
+            
+        };
+    }, [])
+    
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+export default Main
---
+++ redux/actions/index.js
@@ -0,0 +1,21 @@
+import { USER_STATE_CHANGE } from '../constants/index'
+import firebase from 'firebase'
+
+const fetchUser = () => {
+    return((dispatch) => {
+        firebase.firestore()
+            .collection("user")
+            .doc(firebase.auth().currentUser.uid())
+            .get()
+            .then((snapshot) => {
+                if(snapshot.exists){
+                    dispatch({type: USER_STATE_CHANGE, currentUser: snapshot.data()})
+                }
+                else {
+                    console.log('does not exist')
+                }
+            })
+    })
+}
+
+export default fetchUser
