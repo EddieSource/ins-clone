2bad297a810543aa19ae062485b4d28e5feec844
Xianhui Zhu <xz2715@nyu.edu>
2021-06-14 11:44:37 -0400

    make picking image from gallery available

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,19 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
   const [camera, setCamera] = useState(null);
   const [image, setImage] = useState(null)
+  const [type, setType] = useState(Camera.Constants.Type.back)
+
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      const cameraStatus = await Camera.requestPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+
+      const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === 'granted');
     })();
   }, []);
+
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      // image for now not video
+      mediaTypes: ImagePicker.MediaTypeOptions.Image,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
 
   const takePicture = async () => {
     // if camera exists
@@ -23,12 +47,13 @@ export default function App() {
         setImage(data.uri)
     }
   }
-  if (hasPermission === null) {
+  if (hasCameraPermission === null || hasGalleryPermission === null) {
     return <View />;
   }
-  if (hasPermission === false) {
+  if (hasCameraPermission === false || hasGalleryPermission === false) {
     return <Text>No access to camera</Text>;
   }
+
   return (
     <View style={styles.container}>
       <Camera 
@@ -38,6 +63,7 @@ export default function App() {
     //   ratio={'1:1'} // not working on ios
       >
       </Camera>
+
       <Button
             title = 'Flip Image'
             onPress={() => {
@@ -49,16 +75,19 @@ export default function App() {
             }}>
         </Button>
         <Button title='Take a picture' onPress={() => takePicture()} />
-        {image && <Image source={{uri: image}} style={styles.imageContainer}/>}
+        <Button title='Pick Image From Gallery' onPress={() => pickImage()} />
+        
+        {image && <Image source={{uri: image}} style={styles.image}/>}
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  imageContainer: {
+  image: {
       flex: 1,
   },
   camera: {
